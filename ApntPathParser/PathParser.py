f5c1df1044dbd564b2a86b342474c5f99782ae91
Rafael S. Calsaverini <rafael.calsaverini@gmail.com>
2013-02-13 16:19:39 -0200

    fixed bug where path urls with parameters where not correctly parsed

--- ApntPathParser/PathParser.py
+++ ApntPathParser/PathParser.py
@@ -4,7 +4,12 @@ import re
 class ParserNotImplemented(NotImplementedError):
     pass
 
-formats = {'local': strParse.compile("/local/{uf}/{cidade}/{categoria}/{lbsid}/{titulo}.html"),
+class FormatNotRecognized(ValueError):
+    pass
+
+formats = {'local': [strParse.compile("/local/{uf}/{cidade}/{categoria}/{lbsid}/{titulo}.html"),
+                     strParse.compile("/local/{uf}/{cidade}/{categoria}/{lbsid}/{titulo}.html?{headers}")
+                    ],
            'em':    strParse.compile("/em/{uf}_{cidade}/{categoria}/em_{em}")}
 
 def fixSpaces(string):
@@ -20,8 +25,22 @@ def getCorrectParser(pathType):
     except KeyError:
         raise ParserNotImplemented("There's no parser implemented for this kind of url")
 
+def getParsedData(parserobj, path):
+    if isinstance(parserobj, list):
+        for parser in parserobj:
+            parsed = parser.parse(path)
+            if parsed is not None:
+                return parsed
+        raise FormatNotRecognized("Failed to parse this object")
+    else:
+        parsed = parserobj.parse(path)
+        if parsed is None:
+            raise FormatNotRecognized("This")
+        else:
+            return parsed
+
 def parse(path):
     pathType = getPathType(path)
     parser = getCorrectParser(pathType)
-    parsedData = parser.parse(path)
+    parsedData = getParsedData(parser, path)
     return {k: fixSpaces(v) for k,v in parsedData.named.items()}
